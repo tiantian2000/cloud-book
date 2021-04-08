15c93c2961e31f84b90282af9b3fa00c23ef7304
tiantian2000 <1592770232@qq.com>
2021-04-08 09:16:16 +0800

    1

--- common/src/main/java/client/BookClient.java
+++ common/src/main/java/client/BookClient.java
@@ -5,6 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+/**
+ * 测试一下呢
+ */
 @FeignClient(name="book")
 public interface BookClient {
 
